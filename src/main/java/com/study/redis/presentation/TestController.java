530fa8e4f5e001e2deb9182214e72783f54cd35c
오태현 <jeffrey@kai-i.com>
2023-04-06 18:48:54 +0900

    테스트 spring 환경 구성

---
+++ src/main/java/com/study/redis/presentation/TestController.java
@@ -0,0 +1,14 @@
+package com.study.redis.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world!";
+    }
+
+}
